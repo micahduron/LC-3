8407e3645f26cb9c40b4f10b5642e942cc6f763f
Micah Duron <micah@duron.me>
2016-10-25 17:22:17 -0700

    Initial commit of StringView.h.
    I am experimenting with the formatting, so forgive me if it looks a little
    strange.

---
+++ StringView.h
@@ -0,0 +1,60 @@
+#include <string>
+#include <utility>
+
+class StringView {
+public:
+    class StringViewIterator {
+    public:
+        StringViewIterator(const StringViewIterator& other) :
+          m_ptr{ other.m_ptr }
+        {}
+        StringViewIterator(const char* strPtr) :
+          m_ptr{ strPtr }
+        {}
+
+        StringViewIterator operator ++ () {
+            StringViewIterator ret{ *this };
+
+            return ++ret;
+        }
+        StringViewIterator& operator ++ (int) {
+            ++m_ptr;
+
+            return *this;
+        }
+    };
+
+    StringView(const StringView& other) :
+     m_strPtr{ other.m_strPtr },
+     m_length{ other.m_length }
+    {}
+    StringView(const std::string& str, size_t startIndex, size_t strLen) :
+     m_strPtr{ str.data() + startIndex },
+     m_length{ strLen }
+    {}
+    StringView(const char* str, size_t startIndex, size_t strLen) :
+     m_strPtr{ str + startIndex },
+     m_length{ strLen }
+    {}
+
+    char&        operator [] (size_t index)       { return m_strPtr[index]; }
+    const char&  operator [] (size_t index) const { return m_strPtr[index]; }
+
+    char&       at(size_t index);
+    const char& at(size_t index) const;
+
+    size_t size() const { return m_length; }
+
+    iterator       begin() { return iterator{ m_strPtr }; }
+    const_iterator begin() const { return const_iterator{ m_strPtr }; }
+
+    iterator       end() { return iterator{ m_strPtr + m_length }; }
+    const_iterator end() const { return const_iterator{ m_strPtr + m_length }; }
+
+    const_iterator cbegin() const { return this->begin(); }
+    const_iterator cend() const { return this->end(); }
+
+private:
+    const char* m_strPtr;
+    size_t m_length;
+};
