95c1220986d91a77d14bf752554989ae8d8e25c1
Micah Duron <micah@duron.me>
2016-11-15 15:55:39 -0800

    Add Tokenizer::skip*() methods.
    These methods skip through the source string without returning a string view.

--- Tokenizer.h
+++ Tokenizer.h
@@ -1,4 +1,5 @@
 #include <string>
+#include <algorithm>
 #include "StringView.h"
 #include "CharClass.h"
 
@@ -21,6 +22,16 @@ public:
         return *++m_currPtr;
     }
     
+    size_t skip(size_t numChars) {
+        size_t ptrDelta = std::min(numChars, static_cast<size_t>(m_endPtr - m_currPtr));
+
+        m_currPtr += ptrDelta;
+
+        return ptrDelta;
+    }
+    size_t skipUntil(const CharClass& blackList);
+    size_t skipUntilNot(const CharClass& whiteList);
+
     StringView read(size_t numChars) {
         return { m_currPtr += numChars, numChars };
     }
