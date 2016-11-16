fb12ed50e1162ce89f0662cd40cac77b71ca7ec7
Micah Duron <micah@duron.me>
2016-11-15 17:17:53 -0800

    Implement the composition functions introduced in the previous commit.

--- CharClass.cpp
+++ CharClass.cpp
@@ -1,9 +1,27 @@
 #include "CharClass.h"
 
-CharClass operator | (const CharClass& classOne, const CharClass& classTwo) {
+CharClass combine(const CharClass& classOne, const CharClass& classTwo) {
     return {
         [classOne, classTwo] (char c) -> bool {
             return classOne(c) || classTwo(c);
         }
     };
+}
+CharClass intersect(const CharClass& classOne, const CharClass& classTwo) {
+    return {
+        [classOne, classTwo] (char c) -> bool {
+            return classOne(c) && classTwo(c);
+        }
+    };
+}
+CharClass complement(const CharClass& charClass) {
+    return {
+        [charClass] (char c) -> bool {
+            return !charClass(c);
+        }
+    };
+}
+
+CharClass operator | (const CharClass& classOne, const CharClass& classTwo) {
+    return CharClass::combine(classOne, classTwo);
 }
