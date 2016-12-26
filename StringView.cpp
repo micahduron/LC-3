5fe8f995f88c232714581f5a1950a9258919cff9
Micah Duron <micah@duron.me>
2016-12-26 15:20:46 -0800

    Fix behavior in the case of comparing strings of dissimilar length.

--- StringView.cpp
+++ StringView.cpp
@@ -2,6 +2,13 @@
 
 int StringView::compareHelper(const char* str1, size_t str1Size const char* str2,
                               size_t str2Size, compare_func cmpFn) {
+    if (str1Size < str2Size) {
+        return cmpFn('\0', str2[str2Size - 1]);
+    } else if (str1Size > str2Size) {
+        return cmpFn(str1[str1Size - 1], '\0');
+    }
+    auto numIters = str1Size;
+
     while (--numIters) {
         auto cmpResult = cmpFn(*str1++, *str2++);
 
