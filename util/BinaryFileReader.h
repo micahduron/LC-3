fcdeb3dcfef4eb680d9f3e0023312d3a12d1c3a7
Micah Duron <micah@duron.me>
2017-01-01 11:45:05 -0800

    Initial commit of BinaryFileReader.

---
+++ util/BinaryFileReader.h
@@ -0,0 +1,48 @@
+#include <cstdio>
+#include <type_traits>
+#include "BinaryFileIO.h"
+#include "ByteOrder.h"
+
+#pragma once
+
+template <typename Endianness>
+class OrderedBinaryFileReader : public BinaryFileIO<FileMode::Reader> {
+public:
+    using BinaryFileIO::BinaryFileIO;
+
+    template <typename T>
+    size_t read(T* buffer, size_t numElems) {
+        static_assert(std::is_unsigned<T>::value == true,
+                      "Requires unsigned integral type."
+        );
+        size_t elemsRead = 0;
+
+        for (; elemsRead < numElems; ++elemsRead, ++buffer) {
+            if (!get(*buffer)) {
+                break;
+            }
+        }
+        return elemsRead;
+    }
+
+    template <typename T>
+    bool get(T& outputRef) {
+        static_assert(std::is_unsigned<T>::value == true,
+                      "Requires unsigned integral type."
+        );
+        T datum;
+        size_t bytesRead = std::fread(&datum, sizeof(T), 1, file());
+
+        if (bytesRead < sizeof(T)) {
+            seek(-bytesRead, SeekMode::FromCurrent);
+
+            return false;
+        }
+        Endianness converter;
+        outputRef = converter(datum);
+
+        return true;
+    }
+};
+
+using BinaryFileReader = OrderedBinaryFileReader<ByteOrder::SystemOrder>;
