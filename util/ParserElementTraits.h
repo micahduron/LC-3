7dc097ad2000a0b161121d65b64f894732c4ca20
Micah Duron <micah@duron.me>
2017-04-17 16:31:28 -0700

    Use SFINAE to compute ParserElementTraits::IsCompatible
    Suppose a ParserElementTraits template is instantiated with a non-valid
    parser element and IsCompatible is computed. Suppose this invalid class
    has an invalid (even non-existant) parse function. IsCompatible directly
    calls decltype() to determine whether or not a parse function exists and
    has the correct function signature. However, because the invalid class
    has an invalid parse function, the decltype will fail to deduce a type,
    resulting in the compiler throwing an error. What is desired is for
    IsCompatible to simply evaluate to false in this case.
        The desired behavior can be achieved with SFINAE, as type deduction
    failures are explicitly allowed under this system. A simple helper
    class is introduced to implement the SFINAE rules.

--- util/ParserElementTraits.h
+++ util/ParserElementTraits.h
@@ -1,18 +1,31 @@
 #include <type_traits>
+#include <utility>
 
 #pragma once
 
 namespace Util {
+    namespace Internals {
+        template <typename Elem, typename Parser, typename = std::void_t<>>
+        class IsCompatible_Helper : public std::false_type {};
+
+        template <typename Elem, typename Parser>
+        class IsCompatible_Helper<
+          Elem,
+          Parser,
+          std::void_t<
+            decltype(!Elem::parse(std::declval<typename Parser::ContextType&>()))
+          >
+        > : public std::true_type
+        {};
+    }
+
     template <typename Elem>
     struct ParserElementTraits {
         using Context = typename Elem::ContextType;
         using Parent = typename Elem::ParentType;
 
         template <typename TargetParser>
-        static constexpr bool IsCompatible = std::is_same<
-          decltype(Elem::parse(std::declval<typename TargetParser::ContextType&>())),
-          bool
-        >::value;
+        static constexpr bool IsCompatible = Internals::IsCompatible_Helper<Elem, TargetParser>::value;
 
         static_assert(IsCompatible<Parent> == true);
     };
