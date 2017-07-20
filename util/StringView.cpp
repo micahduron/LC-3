a3812a502723c56b30ce20f27552c9b6e6c12517
Micah Duron <micah@duron.me>
2017-07-19 17:38:11 -0700

    Make StringView constexpr friendly
    There are situations where constructing StringViews at compile
    time is useful, necessary even.

--- util/StringView.cpp
+++ util/StringView.cpp
@@ -1,62 +1,11 @@
-#include <cstring>
-#include <cassert>
-#include <utility>
 #include "StringView.h"
 
 namespace Util {
-
-static int LexicographicOrder(char c1, char c2);
-
-StringView::StringView(iterator startIter, iterator endIter) :
-  StringView(startIter.m_currPtr, endIter - startIter)
-{
-    assert(startIter <= endIter);
-}
-
-int StringView::compare(StringView other, compare_func cmpFn) const {
-    size_t numIters = std::min(size(), other.size());
-
-    iterator iterOne = begin();
-    iterator iterTwo = other.begin();
-
-    while (numIters--) {
-        int cmpResult = cmpFn(*iterOne++, *iterTwo++);
-
-        if (cmpResult != 0) {
-            return cmpResult;
-        }
-    }
-    return cmpFn(*iterOne, *iterTwo);
-}
-
-int StringView::compare(StringView other) const {
-    return compare(other, LexicographicOrder);
-}
-
-StringView::iterator StringView::begin() const {
-    return { data(), data() + size() };
-}
-StringView::iterator StringView::end() const {
-    auto endPtr = data() + size();
-
-    return { endPtr, endPtr };
-}
-
-StringView::iterator StringView::cbegin() const {
-    return begin();
-}
-StringView::iterator StringView::cend() const {
-    return end();
-}
 
 std::ostream& operator << (std::ostream& outStream, const StringView& strView) {
     outStream.write(strView.data(), strView.size());
 
     return outStream;
-}
-
-int LexicographicOrder(char c1, char c2) {
-    return c1 - c2;
 }
 
 }
--- util/StringView.h
+++ util/StringView.h
@@ -3,24 +3,133 @@
 #include <cstring>
 #include <cassert>
 #include <utility>
-#include <functional>
 
 #pragma once
 
 namespace Util {
 
+class StringView;
+
+namespace Internals {
+
+class StringView_iterator {
+public:
+    using difference_type = std::ptrdiff_t;
+    using value_type = char;
+    using pointer = const char*;
+    using reference = const char&;
+    using iterator_category = std::random_access_iterator_tag;
+
+    constexpr StringView_iterator(const StringView_iterator& other) = default;
+    constexpr StringView_iterator(StringView_iterator&& other) = default;
+
+    constexpr StringView_iterator(const char* currPtr, const char* endPtr) :
+      m_currPtr{ currPtr },
+      m_endPtr{ endPtr }
+    {}
+
+    constexpr StringView_iterator& operator = (const StringView_iterator& other) = default;
+    constexpr StringView_iterator& operator = (StringView_iterator&& other) = default;
+
+    // Pointer dereference operator
+    constexpr value_type operator * () const {
+        return m_currPtr != m_endPtr ? *m_currPtr : '\0';
+    }
+
+    // Increment/decrement operators
+    constexpr StringView_iterator& operator ++ () {
+        ++m_currPtr;
+
+        return *this;
+    }
+    constexpr StringView_iterator operator ++ (int) {
+        return { m_currPtr++, m_endPtr };
+    }
+
+    constexpr StringView_iterator& operator -- () {
+        --m_currPtr;
+
+        return *this;
+    }
+    constexpr StringView_iterator operator -- (int) {
+        return { m_currPtr--, m_endPtr };
+    }
+
+    // Compound assignment operators
+    constexpr StringView_iterator& operator += (difference_type delta) {
+        m_currPtr += delta;
+
+        return *this;
+    }
+
+    constexpr StringView_iterator& operator -= (difference_type delta) {
+        m_currPtr -= delta;
+
+        return *this;
+    }
+
+    // Arithmetic operators
+    constexpr StringView_iterator operator + (difference_type delta) const {
+        return { m_currPtr + delta, m_endPtr };
+    }
+
+    constexpr StringView_iterator operator - (difference_type delta) const {
+        return { m_currPtr - delta, m_endPtr };
+    }
+
+    // Iterator distance operator
+    constexpr difference_type operator - (const StringView_iterator& other) const {
+        return m_currPtr - other.m_currPtr;
+    }
+
+    // Comparison operators
+    constexpr bool operator == (const StringView_iterator& other) const {
+        return m_currPtr == other.m_currPtr;
+    }
+    constexpr bool operator != (const StringView_iterator& other) const {
+        return !(*this == other);
+    }
+    constexpr bool operator > (const StringView_iterator& other) const {
+        return (*this - other) > 0;
+    }
+    constexpr bool operator < (const StringView_iterator& other) const {
+        return other > *this;
+    }
+    constexpr bool operator >= (const StringView_iterator& other) const {
+        return (*this == other) || (*this > other);
+    }
+    constexpr bool operator <= (const StringView_iterator& other) const {
+        return (*this == other) || (*this < other);
+    }
+
+    // Subscript operator
+    constexpr value_type operator [] (difference_type n) const {
+        return *(*this + n);
+    }
+private:
+    pointer m_currPtr;
+    pointer m_endPtr;
+
+    friend class ::Util::StringView;
+};
+
+}
+
 class StringView {
 public:
-    class iterator;
+    using iterator = Internals::StringView_iterator;
 
-    using compare_func = std::function<int(char, char)>;
-
-    StringView(const StringView& other) = default;
-    StringView(StringView&& other) = default;
-    StringView(const char* str, size_t length) :
+    constexpr StringView(const StringView& other) = default;
+    constexpr StringView(StringView&& other) = default;
+    constexpr StringView(const char* str, size_t length) :
       m_strPtr{ str },
       m_length{ length }
     {}
+    constexpr StringView(iterator startIter, iterator endIter) :
+      StringView(startIter.m_currPtr, endIter - startIter)
+    {
+        assert(startIter <= endIter);
+    }
     StringView(const char* str) :
       StringView{ str, std::strlen(str) }
     {}
@@ -33,154 +142,86 @@ public:
     StringView(const std::string& str) :
       StringView{ str.data(), str.size() }
     {}
-    StringView(iterator startIter, iterator endIter);
 
-    StringView& operator = (const StringView& other) = default;
-    StringView& operator = (StringView&& other) = default;
+    constexpr StringView& operator = (const StringView& other) = default;
+    constexpr StringView& operator = (StringView&& other) = default;
 
-    const char&  operator [] (size_t index) const {
+    constexpr const char&  operator [] (size_t index) const {
         return *(data() + index);
     }
 
-    size_t size() const {
+    constexpr size_t size() const {
         return m_length;
     }
 
-    const char* data() const {
+    constexpr const char* data() const {
         return m_strPtr;
     }
 
-    int compare(StringView other, compare_func cmpFn) const;
+    template <typename Comparator>
+    constexpr int compare(StringView other, Comparator&& cmp = Comparator()) const {
+        size_t numIters = std::min(size(), other.size());
 
-    int compare(StringView other) const;
+        iterator iterOne = begin();
+        iterator iterTwo = other.begin();
 
-    bool operator == (StringView other) const {
+        while (numIters--) {
+            int cmpResult = cmp(*iterOne++, *iterTwo++);
+
+            if (cmpResult != 0) {
+                return cmpResult;
+            }
+        }
+        return cmp(*iterOne, *iterTwo);
+    }
+
+    constexpr int compare(StringView other) const {
+        auto lexicographicCmp = [](char c1, char c2) -> int {
+            return c1 - c2;
+        };
+        return compare(other, lexicographicCmp);
+    }
+
+    constexpr bool operator == (StringView other) const {
         return compare(other) == 0;
     }
 
-    bool operator != (StringView other) const {
+    constexpr bool operator != (StringView other) const {
         return !(*this == other);
     }
 
-    StringView subString(size_t baseOffset, size_t length) {
+    constexpr StringView subString(size_t baseOffset, size_t length) {
         assert(baseOffset <= size());
         assert(length <= (size() - baseOffset));
 
         return { data() + baseOffset, length };
     }
 
-    iterator begin() const;
-    iterator end() const;
+    constexpr iterator begin() const {
+        return { data(), data() + size() };
+    }
+    constexpr iterator end() const {
+        auto endPtr = data() + size();
 
-    iterator cbegin() const;
-    iterator cend() const;
+        return { endPtr, endPtr };
+    }
+
+    constexpr iterator cbegin() const {
+        return begin();
+    }
+    constexpr iterator cend() const {
+        return end();
+    }
 
 private:
     const char* m_strPtr;
     size_t m_length;
 };
 
-class StringView::iterator {
-public:
-    using difference_type = std::ptrdiff_t;
-    using value_type = char;
-    using pointer = const char*;
-    using reference = const char&;
-    using iterator_category = std::random_access_iterator_tag;
-
-    iterator(const iterator& other) = default;
-    iterator(iterator&& other) = default;
-
-    iterator(const char* currPtr, const char* endPtr) :
-      m_currPtr{ currPtr },
-      m_endPtr{ endPtr }
-    {}
-
-    iterator& operator = (const iterator& other) = default;
-    iterator& operator = (iterator&& other) = default;
-
-    // Pointer dereference operator
-    value_type operator * () const {
-        return m_currPtr != m_endPtr ? *m_currPtr : '\0';
-    }
-
-    // Increment/decrement operators
-    iterator& operator ++ () {
-        ++m_currPtr;
-
-        return *this;
-    }
-    iterator operator ++ (int) {
-        return { m_currPtr++, m_endPtr };
-    }
-
-    iterator& operator -- () {
-        --m_currPtr;
-
-        return *this;
-    }
-    iterator operator -- (int) {
-        return { m_currPtr--, m_endPtr };
-    }
-
-    // Compound assignment operators
-    iterator& operator += (difference_type delta) {
-        m_currPtr += delta;
-
-        return *this;
-    }
-
-    iterator& operator -= (difference_type delta) {
-        m_currPtr -= delta;
-
-        return *this;
-    }
-
-    // Arithmetic operators
-    iterator operator + (difference_type delta) const {
-        return { m_currPtr + delta, m_endPtr };
-    }
-
-    iterator operator - (difference_type delta) const {
-        return { m_currPtr - delta, m_endPtr };
-    }
-
-    // Iterator distance operator
-    difference_type operator - (const iterator& other) const {
-        return m_currPtr - other.m_currPtr;
-    }
-
-    // Comparison operators
-    bool operator == (const iterator& other) const {
-        return m_currPtr == other.m_currPtr;
-    }
-    bool operator != (const iterator& other) const {
-        return !(*this == other);
-    }
-    bool operator > (const iterator& other) const {
-        return (*this - other) > 0;
-    }
-    bool operator < (const iterator& other) const {
-        return other > *this;
-    }
-    bool operator >= (const iterator& other) const {
-        return (*this == other) || (*this > other);
-    }
-    bool operator <= (const iterator& other) const {
-        return (*this == other) || (*this < other);
-    }
-
-    // Subscript operator
-    value_type operator [] (difference_type n) const {
-        return *(*this + n);
-    }
-private:
-    pointer m_currPtr;
-    pointer m_endPtr;
-
-    friend class StringView;
-};
-
 std::ostream& operator << (std::ostream& outStream, const StringView& strView);
 
+}
+
+constexpr Util::StringView operator ""_sv (const char* str, size_t strSize) {
+    return { str, strSize };
 }
