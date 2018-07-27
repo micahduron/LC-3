92b49113c4bb9cedd31f186ef6092544595c9e83
Micah Duron <micah@duron.me>
2018-07-27 12:32:01 -0700

    Add new error and warning overloads to Log
    These functions can take in an arbitrary object that can output
    a special prefix before outputting the actual error message.
    For example, an AST node may prefix the error message with
    file location data. By adding the .errorPrefix() method
    to the AST node class, errors related to the AST will have
    a consistent format across the whole codebase.

--- Log.h
+++ Log.h
@@ -6,8 +6,18 @@ namespace LC3 {
 
 class Log {
 public:
+    template <typename T>
+    static std::ostream& error(const T& errObj) {
+        return errObj.errorPrefix(Log::error());
+    }
+
     static std::ostream& error(bool newError = true);
     static size_t errorCount();
+
+    template <typename T>
+    static std::ostream& warning(const T& warnObj) {
+        return warnObj.errorPrefix(Log::warning());
+    }
 
     static std::ostream& warning(bool newWarning = true);
     static size_t warningCount();
