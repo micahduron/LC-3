cd0f7fe2b6d0696e565836069ce146f0e201a535
Micah Duron <micah@duron.me>
2018-08-12 11:32:24 -0700

    Refactor CharClass' test suite

--- tests/CharClass_test.cpp
+++ tests/CharClass_test.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <functional>
 #include <cstring>
 #include <cctype>
 #include <util/CharClass.h>
+#include "UnitTest.h"
 
 using Util::CharClass;
+
+TestFunction MakeTest(std::function<bool()> testFn);
 
 int main() {
     CharClass isAlpha([](char c) -> bool {
@@ -14,14 +18,38 @@ int main() {
     CharClass isLetterJ("J");
     CharClass isNum{ [](char c)->bool { return c >= '0' && c <= '9'; } };
 
-    auto isAlnum = isAlpha | isNum;
+    auto lambdaInit = [isAlpha]() {
+        return isAlpha('a');
+    };
+    UnitTestFn(LambdaInit, MakeTest(lambdaInit));
 
-    auto isNotJ = ~isLetterJ & isAlpha;
+    auto strInit = [isLetterJ]() {
+        return isLetterJ('J') && !isLetterJ('j');
+    };
+    UnitTestFn(TestStrInit, MakeTest(strInit));
 
-    if (isAlnum('a') && isAlnum('5') && isNotJ('a') && !isNotJ('J')) {
-        std::cout << "Yep.\n";
-    } else {
-        std::cout << "Nope.\n";
-    }
-    return 0;
+    auto complementTest = [isNotJ = ~isLetterJ]() {
+        return !isNotJ('J') && isNotJ('j');
+    };
+    UnitTestFn(Complement, MakeTest(complementTest));
+
+    auto unionTest = [isAlnum = isAlpha | isNum]() {
+        return isAlnum('A') && isAlnum('9');
+    };
+    UnitTestFn(Union, MakeTest(unionTest));
+
+    auto intersectTest = []() {
+        CharClass is123 = CharClass("abc123") & CharClass("123xyz");
+
+        return !is123('a') && is123('2') && !is123('z');
+    };
+    UnitTestFn(Intersect, MakeTest(intersectTest));
+
+    return RunTests();
+}
+
+TestFunction MakeTest(std::function<bool()> testFn) {
+    return [testFn](TestContext& t) {
+        t.succeedIf(testFn());
+    };
 }
