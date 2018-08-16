c283231e71818bd74a8a291aa5bff4bac4dc43eb
Micah Duron <micah@duron.me>
2018-08-16 09:52:27 -0700

    Refactor StringTokenizer test suite

--- tests/StringTokenizer_test.cpp
+++ tests/StringTokenizer_test.cpp
@@ -1,51 +1,108 @@
-#include <iostream>
-#include <array>
-#include <vector>
-#include <cstring>
 #include <util/CharClass.h>
 #include <util/StringTokenizer.h>
 #include <util/StringView.h>
+#include "UnitTest.h"
 
 using Util::CharClass;
 using Util::StringTokenizer;
 using Util::StringView;
 
-std::vector<StringView> TokenizeString(const char* str);
+TestFunction TestSkip(const StringView& src, size_t skipCount);
 
 int main() {
-    const char* sourceStr = "One, two, three, four";
-    std::array expectedTokens{ "One", "two", "three", "four" };
-    std::vector<StringView> calculatedTokens = TokenizeString(sourceStr);
+    StringView skipSrc = "abcde"_sv;
 
-    if (calculatedTokens.size() != expectedTokens.size()) {
-        std::cerr << "Unexpected number of tokens (" <<
-            "Calculated: " << calculatedTokens.size() << ", " <<
-            "Expected: " << expectedTokens.size() << ")\n";
-        return 1;
-    }
-    for (size_t i = 0; i < expectedTokens.size(); ++i) {
-        if (calculatedTokens[i] != expectedTokens[i]) {
-            std::cerr << "Unexpected token (" <<
-                "Calculated: " << calculatedTokens[i] << ", " <<
-                "Expected: " << expectedTokens[i] << ")\n";
-            return 1;
-        }
-    }
-    return 0;
+    UnitTestFn(StartPos, TestSkip(skipSrc, 0));
+    UnitTestFn(Skip, TestSkip(skipSrc, skipSrc.size() - 1));
+    UnitTestFn(SkipOvershoot, TestSkip(skipSrc, skipSrc.size() + 100));
+
+    UnitTest(PeekChar, t) {
+        StringTokenizer tok{ "abcd"_sv };
+        tok.skip(2);
+
+        t.succeedIf(tok.peekChar() == 'c');
+    };
+
+    UnitTest(PeekStr, t) {
+        StringTokenizer tok{ "abcxyz"_sv };
+
+        t.succeedIf(tok.peekStr(3, 3) == "xyz");
+    };
+
+    UnitTest(PeekStrOvershoot, t) {
+        StringTokenizer tok{ "abcxyz"_sv };
+
+        t.succeedIf(tok.peekStr(50, 3) == "xyz");
+    };
+
+    UnitTest(PeekCharOvershoot, t) {
+        StringView src = "abcd"_sv;
+        StringTokenizer tok{ src };
+        tok.skip(src.size());
+
+        t.succeedIf(tok.peekChar() == '\0');
+    };
+
+    UnitTest(SkipUntil, t) {
+        StringTokenizer tok{ "aaab"_sv };
+        tok.skipUntil(CharClass("b"));
+
+        t.succeedIf(tok.peekChar() == 'b');
+    };
+
+    UnitTest(SkipUntilNot, t) {
+        StringTokenizer tok{ "aaab" };
+        tok.skipUntilNot(CharClass("a"));
+
+        t.succeedIf(tok.peekChar() == 'b');
+    };
+
+    UnitTest(Read, t) {
+        StringView src = "abcd"_sv;
+        StringTokenizer tok{ src };
+
+        t.succeedIf(tok.read(src.size() - 1) == src.subString(0, src.size() - 1));
+    };
+
+    UnitTest(ReadUntil, t) {
+        StringTokenizer tok{ "abcd wxyz"_sv };
+
+        StringView readResult = tok.readUntil(CharClass(" "));
+
+        t.succeedIf(readResult == "abcd");
+    };
+
+    UnitTest(ReadUntilNot, t) {
+        StringTokenizer tok{ "aaabccc"_sv };
+
+        StringView readResult = tok.readUntilNot(CharClass("a"));
+
+        t.succeedIf(readResult == "aaa");
+    };
+
+    UnitTest(NextToken, t) {
+        StringView src = " a + b + c "_sv;
+        StringTokenizer tok{ src };
+
+        CharClass separators(" +");
+
+        t.succeedIf(
+            tok.nextToken(separators) == "a" &&
+            tok.nextToken(separators) == "b" &&
+            tok.nextToken(separators) == "c"
+        );
+    };
+    return RunTests();
 }
 
-std::vector<StringView> TokenizeString(const char* str) {
-    std::vector<StringView> result;
-    StringTokenizer tokenizer(str);
+TestFunction TestSkip(const StringView& src, size_t skipCount) {
+    return [&src, skipCount](TestContext& t) -> void {
+        StringTokenizer tok{ src };
+        tok.skip(skipCount);
 
-    CharClass isSpace{ [](char c) { return isspace(c); } };
-    CharClass isComma{ [](char c) { return c == ','; } };
+        size_t pos = std::min(src.size(), skipCount);
+        size_t rem = src.size() - pos;
 
-    auto separators = isSpace | isComma;
-
-    while (tokenizer) {
-        StringView numberToken = tokenizer.nextToken(separators);
-        result.push_back(numberToken);
-    }
-    return result;
+        t.succeedIf(tok.position() == pos && tok.remaining() == rem);
+    };
 }
